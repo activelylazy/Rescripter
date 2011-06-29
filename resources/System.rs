daeac5cfab265b32824d0ec607afaade0ac5fba5
David <dave@activelylazy.co.uk>
2011-06-29 18:23:58 +0100

    Merged into single static / non-static approaches into single replace constructor call

--- resources/System.rs
+++ resources/System.rs
@@ -7,7 +7,7 @@ function findMethodByName(type, methodName) {
 	}
 }
 
-function replaceConstructorCall(constructor, newMethodName) {
+function replaceConstructorCall(constructor, newMethodName, useStaticImport) {
 	var references = Find.referencesTo(constructor);
 	
 	for(var i=0; i<references.length; i++) {
@@ -17,30 +17,34 @@ function replaceConstructorCall(constructor, newMethodName) {
                                                         references[i].getOffset(),
                                                         references[i].getLength())
                             .getOffset();
-        ChangeText.inCompilationUnit(references[i].getElement().getCompilationUnit(),
-                                     startOfNew, endOfCons-startOfNew,
-                                     constructor.getDeclaringType().getElementName()+"."+newMethodName);
+                            
+        if (useStaticImport) {
+	        ChangeText.inCompilationUnit(references[i].getElement().getCompilationUnit(),
+	                                     startOfNew, endOfCons-startOfNew,
+	                                     newMethodName);
+	    	addImport(references[i].getElement().getCompilationUnit(),
+	    			  "import static "+constructor.getDeclaringType().getFullyQualifiedName()+"."+newMethodName+";");
+        } else {
+	        ChangeText.inCompilationUnit(references[i].getElement().getCompilationUnit(),
+	                                     startOfNew, endOfCons-startOfNew,
+	                                     constructor.getDeclaringType().getElementName()+"."+newMethodName);
+        }
 	}
 }
 
-function replaceConstructorCallWithStaticallyImportedCall(constructor, newMethodName) {
-    var references = Find.referencesTo(constructor);
-    
-    for(var i=0; i<references.length; i++) {
-        var startOfNew = references[i].offset;
-        var endOfCons = ASTTokenFinder.findTokenOfType(references[i].getElement().getCompilationUnit(),
-                                                        org.eclipse.jdt.core.compiler.ITerminalSymbols.TokenNameLPAREN,
-                                                        references[i].getOffset(),
-                                                        references[i].getLength())
-                            .getOffset();
-        
-        var lastImport = getLastImportPosition(references[i].getElement().getCompilationUnit());
-        ChangeText.inCompilationUnit(references[i].getElement().getCompilationUnit(),
-                                     startOfNew, endOfCons-startOfNew,
-                                     newMethodName);
-
-        ChangeText.inCompilationUnit(references[i].getElement().getCompilationUnit(),
-                                     lastImport, 0,
-                                     "\nimport static "+constructor.getDeclaringType().getFullyQualifiedName()+"."+newMethodName+";");
-    }
+function addImport(compilationUnit, importText) {
+	var imports = compilationUnit.getImports();
+	var lastImport = 0;
+	for(var i=0; i<imports.length; i++) {
+		var pos = imports[i].getSourceRange().getOffset() + imports[i].getSourceRange().getLength();
+		if (pos > lastImport) {
+			lastImport = pos;
+		}
+	}
+	if (lastImport == 0) {
+		lastImport = compilationUnit.getPackageDeclarations()[0].getSourceRange().getOffset() +
+					 compilationUnit.getPackageDeclarations()[0].getSourceRange().getLength();
+	}
+	ChangeText.inCompilationUnit(compilationUnit, lastImport, 0,
+								 "\r\n"+importText);
 }
