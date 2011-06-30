3a6719d8a7f90b020e41034ca9f3e40a579dcfda
David Green <dave@activelylazy.co.uk>
2011-06-30 17:16:29 +0100

    Updated getting started script

--- demo/script/GettingStarted.rs
+++ demo/script/GettingStarted.rs
@@ -3,6 +3,26 @@
 
 //Alert.info(Find.typeByName("Person").getFullyQualifiedName());
 
+/*
 var type = Find.typeByName("Person");
 Alert.info(type.getMethod("getName",[]).getElementName());
 Alert.info(findMethodByName(type, "getName").getElementName());
+*/
+
+/*
+var type = Find.typeByName("Person");
+var method = type.getMethod("getName",[]);
+var references = Find.referencesTo(method);
+Alert.info("There are "+references.length+" references to getName()");
+*/
+
+var type = Find.typeByName("Person");
+var method = type.getMethod("getName",[]);
+var references = Find.referencesTo(method);
+Alert.info(ASTTokenFinder.findTokenOfType(type.getCompilationUnit(), org.eclipse.jdt.core.compiler.ITerminalSymbols.TokenNameLPAREN, 
+                                references[0].getOffset(), references[0].getLength()));
+
+//var type = Find.typeByName("Person");
+//var method = type.getMethod("getName",[]);
+//ChangeText.inCompilationUnit(type.getCompilationUnit(), method.getSourceRange().getOffset(), 0,
+//                            "/* This is a comment */\n\t"); 
