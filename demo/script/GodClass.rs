1c2cc7eb605025a3126ce857727ef71d008fc6ad
David <dave@activelylazy.co.uk>
2011-06-30 18:47:56 +0100

    Changed god class example to change field name of each reference

--- demo/script/GodClass.rs
+++ demo/script/GodClass.rs
@@ -25,11 +25,50 @@ function moveMethodBetweenInjectables() {
 	for(var i=0; i<references.length; i++) {
 		var refType = references[i].getElement().getDeclaringType();
 		if (!typeDefinesFieldOfType(refType, targetService)) {
+			changeReferenceFieldTo(references[i], initLowerCase(targetService.getElementName()));
 			addField(refType, targetService);
 			addImport(refType.getCompilationUnit(), targetService);
 			refType.getCompilationUnit().commitWorkingCopy(true, null);
 		}
 	}
+}
+
+function debugElement(element) {
+	Alert.info("Have: "+element.getCompilationUnit().getSource().substring(element.getOffset(), reference.getOffset()+reference.getLength()));
+}
+var lastIdentifierOffset;
+var lastIdentifierLength;
+var found = false;
+function changeReferenceFieldTo(reference, newField) {
+	ASTTokenFinder.scanTokens(reference.getElement().getCompilationUnit(), 
+								reference.getElement().getSourceRange().getOffset(), 
+								reference.getOffset() - reference.getElement().getSourceRange().getOffset(),
+		function(tokenType, offset, length) {
+			if (!found && tokenType == org.eclipse.jdt.core.compiler.ITerminalSymbols.TokenNameIdentifier) {
+				if (offset == reference.getOffset()) {
+					found = true;
+					return;
+				}
+				lastIdentifierOffset = offset;
+				lastIdentifierLength = length;
+			}
+		}
+	);
+
+	if (lastIdentifierLength == 0) {
+		Alert.error("Couldn't figure out the field calling the method to move");
+		return;
+	}
+	ChangeText.inCompilationUnit(reference.getElement().getCompilationUnit(),
+									lastIdentifierOffset, lastIdentifierLength, newField);
+/*
+    var fieldRange =  ASTTokenFinder.findTokenOfType(reference.getElement().getCompilationUnit(),
+                                                           org.eclipse.jdt.core.compiler.ITerminalSymbols.TokenNameDOT,
+                                                           reference.getOffset(),
+                                                           reference.getLength());
+	ChangeText.inCompilationUnit(reference.getElement().getCompilationUnit(),
+								 fieldRange.getOffset(), fieldRange.getLenght(), newField);
+*/
 }
 
 function addImport(compilationUnit, importType) {
