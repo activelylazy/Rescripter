96964e9f650375f7d236ae468999802f455f34eb
David Green <dave@activelylazy.co.uk>
2011-07-06 15:42:31 +0100

    Removed now unused replace constructor system call with replace method

--- com.rescripter.editor/resources/System.rs
+++ com.rescripter.editor/resources/System.rs
@@ -183,33 +183,12 @@ function filter(from, test) {
 	return results;
 }
 
-function replaceConstructorCall(constructor, newMethodName, useStaticImport) {
-	var newMethod = constructor.getDeclaringType().getMethod(newMethodName, constructor.getParameterTypes());
-	if (newMethod.getSignature() == undefined) {
-		Alert.error("Failed to find "+constructor.getDeclaringType().getFullyQualifiedName()+"."+newMethodName);
-	}
-	var references = SearchHelper.findReferencesTo(constructor);
-	
-	for(var i=0; i<references.length; i++) {
-		var startOfNew = references[i].offset;
-        var endOfCons = ASTTokenFinder.findTokenOfType(references[i].getElement().getCompilationUnit(),
-                                                       org.eclipse.jdt.core.compiler.ITerminalSymbols.TokenNameLPAREN,
-                                                       references[i].getOffset(),
-                                                       references[i].getLength())
-                            .getOffset();
-                            
-        if (useStaticImport) {
-	        ChangeText.inCompilationUnit(references[i].getElement().getCompilationUnit(),
-	                                     startOfNew, endOfCons-startOfNew,
-	                                     newMethodName);
-        	references[i].getElement().getCompilationUnit().createImport(
-        			constructor.getDeclaringType().getFullyQualifiedName()+"."+newMethodName,
-        			null, org.eclipse.jdt.core.Flags.AccStatic, null);
-        	references[i].getElement().getCompilationUnit().commitWorkingCopy(true, null);
-        } else {
-	        ChangeText.inCompilationUnit(references[i].getElement().getCompilationUnit(),
-	                                     startOfNew, endOfCons-startOfNew,
-	                                     constructor.getDeclaringType().getElementName()+"."+newMethodName);
-        }
-	}
+function replaceMethodCall(cu, offset, length, newCall) {
+    var startOfNew = offset;
+    var endOfCons = ASTTokenFinder.findTokenOfType(cu,
+                                                   org.eclipse.jdt.core.compiler.ITerminalSymbols.TokenNameLPAREN,
+                                                   offset,
+                                                   length)
+                        .getOffset();
+    return new org.eclipse.text.edits.ReplaceEdit(startOfNew, endOfCons-startOfNew, newCall);
 }
