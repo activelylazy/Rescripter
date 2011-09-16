8da9f2acae96f0922af259ef0ee3f47d7cdbc25f
Peter Kofler <peter.kofler@code-cop.org>
2011-09-16 12:41:23 +0200

    use copy of SourceRange to be compatible with 3.5 and 3.6

--- com.rescripter.editor/src/com/rescripter/syntax/ASTTokenFinder.java
+++ com.rescripter.editor/src/com/rescripter/syntax/ASTTokenFinder.java
@@ -1,20 +1,20 @@
 package com.rescripter.syntax;
 
 import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.ISourceRange;
 import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.core.ToolFactory;
 import org.eclipse.jdt.core.compiler.IScanner;
 import org.eclipse.jdt.core.compiler.ITerminalSymbols;
 import org.eclipse.jdt.core.compiler.InvalidInputException;
-import org.eclipse.jdt.internal.core.SourceRange;
 
 public class ASTTokenFinder {
 
-    public SourceRange findIdentifier(ICompilationUnit theCU, int offset, int length) throws JavaModelException {
+    public ISourceRange findIdentifier(ICompilationUnit theCU, int offset, int length) throws JavaModelException {
         return findTokenOfType(theCU, ITerminalSymbols.TokenNameIdentifier, offset, length);
     }
     
-    public SourceRange findTokenOfType(ICompilationUnit theCU, int tokenType, int offset, int length) throws JavaModelException {
+    public ISourceRange findTokenOfType(ICompilationUnit theCU, int tokenType, int offset, int length) throws JavaModelException {
         IScanner scanner = ToolFactory.createScanner(false, false, false, false);
         scanner.setSource(theCU.getSource().toCharArray());
         scanner.resetTo(offset, offset + length);
@@ -51,4 +51,50 @@ public class ASTTokenFinder {
         }
     }
     
+}
+
+/**
+ * Copied SourceRange to be compatible with Eclipse 3.5 and 3.6.
+ */
+class SourceRange implements ISourceRange {
+
+	protected final int offset, length;
+
+	public SourceRange(int offset, int length) {
+		this.offset = offset;
+		this.length = length;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (!(obj instanceof ISourceRange)) {
+			return false;
+		}
+		ISourceRange sourceRange = (ISourceRange) obj;
+		return sourceRange.getOffset() == this.offset && sourceRange.getLength() == this.length;
+	}
+
+	public int getLength() {
+		return this.length;
+	}
+
+	public int getOffset() {
+		return this.offset;
+	}
+
+	@Override
+	public int hashCode() {
+		return this.length ^ this.offset;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder buffer = new StringBuilder();
+		buffer.append("[offset="); //$NON-NLS-1$
+		buffer.append(this.offset);
+		buffer.append(", length="); //$NON-NLS-1$
+		buffer.append(this.length);
+		buffer.append("]"); //$NON-NLS-1$
+		return buffer.toString();
+	}
 }
