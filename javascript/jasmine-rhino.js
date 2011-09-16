118580455906e2ad27976ae740f51d7f8b1f30b9
David Green <dave@activelylazy.co.uk>
2011-09-16 10:41:13 +0100

    Added jasmine/rhino support and small helper function to make running jasmine in rescripter a bit easier

---
+++ javascript/jasmine-rhino.js
@@ -0,0 +1,35 @@
+/*
+ * Provide missing globals for jasmine when running under rhino
+ * Include just this, which in turn includes jasmine.
+ */
+(function (global) { 
+    var timer = new java.util.Timer(); 
+    var counter = 1; 
+    var ids = {}; 
+
+    global.setTimeout = function (fn, delay) { 
+        var id = counter++; 
+        ids[id] = new JavaAdapter(java.util.TimerTask, { run: fn }); 
+        timer.schedule(ids[id], delay); 
+        return id; 
+    }; 
+
+    global.clearTimeout = function (id) { 
+        ids[id].cancel(); 
+        timer.purge(); 
+        delete ids[id]; 
+    }; 
+
+    global.setInterval = function (fn, delay) { 
+        var id = counter++; 
+        ids[id] = new JavaAdapter(java.util.TimerTask, { run: fn }); 
+        timer.schedule(ids[id], delay, delay); 
+        return id; 
+    }; 
+
+    global.clearInterval = global.clearTimeout;
+    
+    global.exports = {};
+
+})(this); 
+
