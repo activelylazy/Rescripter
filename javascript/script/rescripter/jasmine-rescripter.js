5c653fda43cd57e99cdd8c917d0fbf91a55210ed
Peter Kofler <peter.kofler@code-cop.org>
2011-09-21 01:03:00 +0200

    update script to lates version from example

--- javascript/script/rescripter/jasmine-rescripter.js
+++ javascript/script/rescripter/jasmine-rescripter.js
@@ -1,27 +1,30 @@
 
 function runJasmine() {
-	var jasmineEnv = jasmine.getEnv();
-	jasmineEnv.updateInterval = 1000;
-	var trivialReporter = new RescripterReporter();
-	jasmineEnv.addReporter(trivialReporter);
-	jasmineEnv.execute();
-	while(!trivialReporter.finished) {
-	    java.lang.Thread.sleep(1000);
-	}
-	
-	if (trivialReporter.failed) {
-		Debug.message("Messages are: "+trivialReporter.messages);
-	    Alert.info("Test failed\n"+trivialReporter.messages.join("\n\n"));
-	} else {
-	    Alert.info("Tests passed");
-	}
-}	
+    var jasmineEnv = jasmine.getEnv();
+    jasmineEnv.updateInterval = 1000;
+    var trivialReporter = new RescripterReporter();
+    TestResult.startTest();
+    jasmineEnv.addReporter(trivialReporter);
+    jasmineEnv.execute();
+    while(!trivialReporter.finished) {
+        TestResult.updateResults();
+        java.lang.Thread.sleep(500);
+    }
+    TestResult.updateResults();
+    
+    if (trivialReporter.failed) {
+        Debug.message("Messages are: "+trivialReporter.messages);
+        Alert.info("Test failed\n"+trivialReporter.messages.join("\n\n"));
+    } else {
+        Alert.info("Tests passed");
+    }
+}   
 
 function RescripterReporter() {
     this.messages = [];
-	this.failed = false;
-	this.finished = false;
-	
+    this.failed = false;
+    this.finished = false;
+    
     this.reportRunnerStarting = function(runner) {
     };
         
@@ -35,11 +38,22 @@ function RescripterReporter() {
     this.reportSpecStarting = function(spec) {
     };
     
+    this.collateFailures = function(spec) {
+        var messages = [];
+        foreach(spec.results().getItems(), function(item) {
+            if (item != "Passed.") {
+                messages.push(item); 
+            }
+        });
+        return messages.join(", ");
+    };
+    
     this.reportSpecResults = function(spec) {
         this.failed = this.failed || spec.results().failedCount > 0;
         if (this.failed) {
-        	this.messages.push(spec.suite.description+" "+spec.description+": "+spec.results().getItems());	
+            this.messages.push(spec.suite.description+" "+spec.description+": "+this.collateFailures(spec));    
         }
+        TestResult.queueResult(spec.suite.description, spec.description, this.collateFailures(spec));
     };
     
     this.log = function(msg) {
